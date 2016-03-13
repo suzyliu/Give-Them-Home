9ba31d06ee074d997a620e0996517512b974dd8d
SzuYu Liu <szuyu96@gmail.com>
2016-03-13 16:18:06 -0700

    Update the UI and new functions for display text in detail page.

--- js/detailpage.js
+++ js/detailpage.js
@@ -13,10 +13,40 @@ $(document).ready(function(){
     } 
     getparm();
     
-//    var _img = document.getElementById('main_image');
-//    var newImg = new Image;
-//    newImg.onload = function() {
-//        _img.src = this.src;
-//    }
-//    newImg.src = "images/"+main_role+".jpg";
+    var _img = document.getElementById('main_image');
+    if (_img) {
+        var newImg = new Image;
+        newImg.onload = function() {
+            _img.src = this.src;
+        }
+        newImg.src = "images/"+main_role+".jpg";
+        
+        var text = document.getElementById('name');
+        text.textContent = text.textContent + main_role;
+    }
+//    detailContent
+    var filename = main_role +".text";
+    readTextFile(filename);
+
+    function readTextFile(file)
+    {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function ()
+        {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status == 0)
+                {
+                    var allText = rawFile.responseText;
+//                    alert(allText);
+                    
+                    var detail = document.getElementById('detailContent');
+                    detail.textContent = allText;
+                }
+            }
+        }
+        rawFile.send(null);
+    }  
 });
+    
